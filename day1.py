11690d952d1651a2bdafaacc10e727f372d6b84b
Thomas Wilson <thomas.wilson@daveramsey.com>
2020-12-03 10:29:52 -0600

    add final solve for day1 problem 1

--- day1.py
+++ day1.py
@@ -23,3 +23,8 @@ def solve_for_2020(list_of_nums):
 def string_to_list_of_ints(string):
     list_of_nums = string.split('\n')
     return [int(num) for num in list_of_nums]
+
+def solve_problem_1(input):
+    list_of_ints = string_to_list_of_ints(string)
+    sum_2020_nums = solve_for_2020(list_of_ints)
+    return sum_2020_nums[0] * sum_2020_nums[1]
